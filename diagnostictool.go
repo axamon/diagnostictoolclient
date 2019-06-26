da961f31180f35bc0ede99077d5a94f1deb2f145
Alberto Bregliano <alberto.bregliano@pm.me>
2019-06-27 01:18:53 +0200

    added context

--- diagnostictool.go
+++ diagnostictool.go
@@ -1,132 +1,142 @@
+// Copyright (c) 2019 Alberto Bregliano
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 package main
 
 import (
-        "fmt"
-        "encoding/json"
-        "regexp"
-        "crypto/tls"
-        "io/ioutil"
-        "log"
-        "net/http"
-        "os"
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
 )
 
 const (
-        endpointTgu = "https://10.38.34.138:8443/DiagnosticTool/api.php?method=DiagnosticTool&sincrono=N&format=json&tgu="
-        endpointEsito = "https://10.38.34.138:8443/DiagnosticTool/api.php?method=DiagnosticTool&sincrono=Y&format=json&cod_esito="
+	endpointTgu   = "https://10.38.34.138:8443/DiagnosticTool/api.php?method=DiagnosticTool&sincrono=N&format=json&tgu="
+	endpointEsito = "https://10.38.34.138:8443/DiagnosticTool/api.php?method=DiagnosticTool&sincrono=Y&format=json&cod_esito="
 )
 
 type response struct {
-Esito string  `json:"esito"`
-TDResponseCod string  `json:"responsecode"`
-TDResponse string  `json:"response"`
-CodEsito string   `json:"cod_esito"`
+	Esito         string `json:"esito"`
+	TDResponseCod string `json:"responsecode"`
+	TDResponse    string `json:"response"`
+	CodEsito      string `json:"cod_esito"`
 }
-
-
 
 var re = regexp.MustCompile(`(?m)^\d+$`)
 
-func DiagnosticToolClient(tgu string) (result string, err error){
+func DiagnosticToolClient(ctx context.Context, tgu string) (result string, err error) {
 
-        fmt.Println(tgu)
+	fmt.Println(tgu)
 
-        // Costringe il client ad accettare anche certificati https non validi
-        // o scaduti.
-        transCfg := &http.Transport{
-                // Ignora certificati SSL scaduti.
-                TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-        }
+	// Costringe il client ad accettare anche certificati https non validi
+	// o scaduti.
+	transCfg := &http.Transport{
+		// Ignora certificati SSL scaduti.
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 
-        client := &http.Client{Transport: transCfg}
-        url := endpointTgu + tgu
+	client := &http.Client{Transport: transCfg}
+	url := endpointTgu + tgu
 
-        fmt.Println(url)
+	fmt.Println(url)
 
-        req, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-                log.Printf("ERROR Impossibile creare richiesta: %s\n", err.Error())
-        }
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("ERROR Impossibile creare richiesta: %s\n", err.Error())
+	}
 
+	username := os.Getenv("DiagnosticToolUsername")
+	password := os.Getenv("DiagnosticToolPassoword")
 
-        username := os.Getenv("DiagnosticToolUsername")
-        password := os.Getenv("DiagnosticToolPassoword")
+	req.SetBasicAuth(username, password)
 
-        req.SetBasicAuth(username, password)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("ERROR Impossibile inviare richiesta http: %s\n", err.Error())
+	}
+	//defer resp.Body.Close()
 
-        resp, err := client.Do(req)
-        if err != nil {
-                log.Printf("ERROR Impossibile inviare richiesta http: %s\n", err.Error())
-        }
-        //defer resp.Body.Close()
+	responsBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("ERROR Impossibile leggere body reqest: %s\n", err.Error())
+	}
 
-        responsBody, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                log.Printf("ERROR Impossibile leggere body reqest: %s\n", err.Error())
-        }
+	fmt.Println(string(responsBody))
 
+	risposta := new(response)
 
-                fmt.Println(string(responsBody))
+	err = json.Unmarshal(responsBody, &risposta)
+	if err != nil {
+		log.Println(err)
+	}
 
+	fmt.Println(risposta)
 
-                risposta := new(response)
+	dati, err := dt(risposta.CodEsito)
+	if err != nil {
+		log.Println(err)
+	}
 
-
-                err = json.Unmarshal(responsBody, &risposta)
-                if err != nil {
-                        log.Println(err)
-                }
-
-                fmt.Println(risposta)
-      
-
-        dati, err :=  dt(risposta.CodEsito)
-            if err != nil {
-                        log.Println(err)
-                }
-
-        return dati, err
+	return dati, err
 
 }
 
+func dt(code string) (str string, err error) {
 
+	// Costringe il client ad accettare anche certificati https non validi
+	// o scaduti.
+	transCfg := &http.Transport{
+		// Ignora certificati SSL scaduti.
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 
-func dt(code string) (str string, err error){
+	client := &http.Client{Transport: transCfg}
+	url := endpointEsito + code
 
+	fmt.Println(url)
 
-        // Costringe il client ad accettare anche certificati https non validi
-        // o scaduti.
-        transCfg := &http.Transport{
-                        // Ignora certificati SSL scaduti.
-                        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-        }
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("ERROR Impossibile creare richiesta: %s\n", err.Error())
+	}
 
-        client := &http.Client{Transport: transCfg}
-        url := endpointEsito + code
+	username := os.Getenv("DiagnosticToolUsername")
+	password := os.Getenv("DiagnosticToolPassoword")
 
-        fmt.Println(url)
+	req.SetBasicAuth(username, password)
 
-        req, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-                        log.Printf("ERROR Impossibile creare richiesta: %s\n", err.Error())
-        }
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("ERROR Impossibile inviare richiesta http: %s\n", err.Error())
+	}
+	//defer resp.Body.Close()
 
+	responsBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("ERROR Impossibile leggere body reqest: %s\n", err.Error())
+	}
 
-        username := os.Getenv("DiagnosticToolUsername")
-        password := os.Getenv("DiagnosticToolPassoword")
-
-        req.SetBasicAuth(username, password)
-
-        resp, err := client.Do(req)
-        if err != nil {
-                        log.Printf("ERROR Impossibile inviare richiesta http: %s\n", err.Error())
-        }
-        //defer resp.Body.Close()
-
-        responsBody, err := ioutil.ReadAll(resp.Body)
-        if err != nil {
-                        log.Printf("ERROR Impossibile leggere body reqest: %s\n", err.Error())
-        }
-
-        return string(responsBody), err
+	return string(responsBody), err
 }
--- main.go
+++ main.go
@@ -1,73 +1,112 @@
+// Copyright (c) 2019 Alberto Bregliano
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 package main
 
 import (
-        "fmt"
-        "log"
-        "net/http"
-        "encoding/base64"
-        "strings"
+	"context"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 )
+
+var port = flag.String("p", ":8080", "porta da usare, default :8080")
 
 func main() {
 
-        certPath := "server.pem"
-        keyPath := "server.key"
+	flag.Parse()
 
-//      api := NewAPI(certPath, keyPath)
+	// Crea il contesto
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	// Certificati per https
+	// da creare con i comandi:
+	//  openssl genrsa -out server.key 2048
+	//  openssl ecparam -genkey -name secp384r1 -out server.key
+	//  openssl req -new -x509 -sha256 -key server.key -out server.pem -days 3650
+	certPath := "server.pem"
+	keyPath := "server.key"
 
-        http.HandleFunc("/diagnostictool", func (w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/diagnostictool", func(w http.ResponseWriter, r *http.Request) {
+		r.WithContext(ctx)
 
-                if basicAuth(w, r) {
-                keys, _ := r.URL.Query()["tgu"]
-                result, err := DiagnosticToolClient(keys[0])
-                if err !=nil {
-                        log.Println("funzione DiagnsticToolClient in errore: ", err.Error())
+		switch ok := basicAuth(w, r); ok {
+		// Se correttamente autenticato
+		case true:
+			keys, _ := r.URL.Query()["tgu"]
 
-                }
-                fmt.Fprint(w, result+"\n")
-                return
-                }
-                w.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
-                w.WriteHeader(401)
-                w.Write([]byte("401 Unauthorized\n"))
-                return
-        })
+			// result contiene il json con i dati.
+			result, err := DiagnosticToolClient(ctx, keys[0])
+			if err != nil {
+				log.Println("funzione DiagnsticToolClient in errore: ", err.Error())
+			}
 
+			// invia il json con i dati in risposta.
+			fmt.Fprint(w, result+"\n")
+			return
 
-        err :=  http.ListenAndServeTLS(":8080", certPath, keyPath, nil)
-//        err :=  http.ListenAndServe(":8080",nil)
-        if err != nil {
-                log.Fatal("ListenAndServe: ", err)
-        }
+		// Se NON correttamente autenticato
+		case false:
+			w.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
+			w.WriteHeader(401)
+			w.Write([]byte("401 Unauthorized\n"))
+			return
+		}
+	})
+
+	err := http.ListenAndServeTLS(*port, certPath, keyPath, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
-
+// basicAuth gestisce l'autenticazione all'app.
 func basicAuth(w http.ResponseWriter, r *http.Request) bool {
 
-        auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+	auth := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 
-        if len(auth) != 2 || auth[0] != "Basic" {
-                log.Println("Errore non Basic e non user e pass")
-            http.Error(w, "authorization failed", http.StatusUnauthorized)
-            return false
-        }
+	if len(auth) != 2 || auth[0] != "Basic" {
+		log.Println("Errore non Basic e non user e pass")
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return false
+	}
 
-        payload, _ := base64.StdEncoding.DecodeString(auth[1])
-        pair := strings.SplitN(string(payload), ":", 2)
+	payload, _ := base64.StdEncoding.DecodeString(auth[1])
+	pair := strings.SplitN(string(payload), ":", 2)
 
-        if len(pair) != 2 || !validate(pair[0], pair[1]) {
-                log.Println("Errore nella validazione user e pass")
-            http.Error(w, "authorization failed", http.StatusUnauthorized)
-            return false
-        }
+	if len(pair) != 2 || !validate(pair[0], pair[1]) {
+		log.Println("Errore nella validazione user e pass")
+		http.Error(w, "authorization failed", http.StatusUnauthorized)
+		return false
+	}
 
-        return true
-    }
+	return true
+}
 
 func validate(username, password string) bool {
-    if username == "test" && password == "test" {
-        return true
-    }
-    return false
+	if username == "test" && password == "test" {
+		return true
+	}
+	return false
 }
